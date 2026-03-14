003be071593702c5d9d138331ab10181e54793ef
bhargav-4421 <gorujayabhargavamallesh@gmail.com>
2026-03-14 10:27:33 +0530

    Add Solution class for Valid Parentheses problem
    Implement a solution for validating parentheses using a stack.

--- DAY-1/Valid Parentheses.py
+++ DAY-1/Valid Parentheses.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def isValid(self,s):
+        stack=[]
+        for b in s:
+            if b=="(":
+                stack.append(")")
+            elif b=="{":
+                stack.append("}")
+            elif b=="[":
+                stack.append("]")
+            elif len(stack)==0 or b!=stack.pop():
+                return False
+        return len(stack)==0
