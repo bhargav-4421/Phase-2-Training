3f41ffffe04aa6a33dc40ee0661f39a866eed73e
bhargav-4421 <gorujayabhargavamallesh@gmail.com>
2026-03-14 10:35:28 +0530

    Implement inorder traversal for binary tree

---
+++ DAY-3/Binary Tree Inorder Traversal.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        res = []
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                res.append(node.val)
+                inorder(node.right)
+        inorder(root)
+        return res
